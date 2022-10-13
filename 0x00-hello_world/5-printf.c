378ee04007b9b70ed345d8ffa4851c6165de5086
Collins-13 <collinsnosa13@gmail.com>
2022-10-13 15:25:30 -0700

    pfint edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry
  * Return: Always 0 (task5)
  */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
