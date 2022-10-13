648cccab352af8cc0b5fcb609fe3074c2b6205c3
Collins-13 <collinsnosa13@gmail.com>
2022-10-13 14:57:30 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+ ** main - Entry
+ ** Return: Always 0
+ **/
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
