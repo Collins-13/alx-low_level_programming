b586c2f9c9b7d2905943714a1db2c946ec4d6933
Collins-13 <collinsnosa13@gmail.com>
2022-10-13 15:23:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - Entry
+ *main - Entry point
  *Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
