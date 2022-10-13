dfcef252dbdc8c8f23a8aa5e2395b19aae647b9e
Collins-13 <collinsnosa13@gmail.com>
2022-10-13 13:50:59 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- * main - printing file
- *
- * Return: Always 0 (printed)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art");
+	puts("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
