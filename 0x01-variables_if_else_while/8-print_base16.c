a306aa98141797b63b59ae7f398df27f69307245
Collins-13 <collinsnosa13@gmail.com>
2022-10-14 15:55:05 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (successful)
  */
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	int d;
 
 	c = 'a';
-	d = 0;
+	d = '0';
 	while
 		(d < 10) {
 			putchar(d + '0');
