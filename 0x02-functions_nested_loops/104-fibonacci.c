8ed937bd969830147fc29d71a564c9720f5578d5
Collins-13 <collinsnosa13@gmail.com>
2022-10-19 14:46:28 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
@@ -20,7 +19,7 @@ int main(void)
 		printf("%lu, ", sum);
 
 		fib1 = fib2;
-		fib2 = sun;
+		fib2 = sum;
 	}
 
 	fib1_half1 = fib1 / 10000000000;
