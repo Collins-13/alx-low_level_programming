6d8afa8bc0a36936097418049474e2058cbb0216
Collins-13 <collinsnosa13@gmail.com>
2022-10-24 13:22:09 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+
+/**
+ * swap_int - a function that swaps the values of two integers.
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int s;
+	s = *a;
+	*a = *b;
+	*b = s;
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every other character of a string, starting with the first character, followed by a new line.
+ * @str: char to check
+ * Return: 0
+ */
+
+void puts2(char *str)
+
+{
+	int string;
+	for (string = 0; str[string]
+			if (string % 2 == 0)
+			_putchar(str[string}}
+				_putchar('\n');
+}
