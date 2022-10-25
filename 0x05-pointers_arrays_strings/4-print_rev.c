59240211524066293c25dd415be80dfdd33d6658
Collins-13 <collinsnosa13@gmail.com>
2022-10-25 12:08:21 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,16 @@
 void print_rev(char *s)
 
 {
-	int i = 0;
+	int i, n;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
 
 	_putchar('\n');
 }
