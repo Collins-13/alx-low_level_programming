253c21617c922711ac02933a4c8c3db025701588
Collins-13 <collinsnosa13@gmail.com>
2022-10-27 08:13:20 -0700

    putchar

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,7 +27,7 @@ void print_line(char *c, int s, int l)
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[1 * 10 + k]);
 		else
-			putchar('-');
+			putchar('.');
 	}
 }
 
@@ -44,7 +44,7 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("%08x; ", i * 10);
+		printf("%08x: ", i * 10);
 		if (i < size / 10)
 		{
 			print_line(b, 9, i);
