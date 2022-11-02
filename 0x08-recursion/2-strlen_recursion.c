7decefa5ed2673edd39c858dfa6af90e6c738d35
Collins-13 <collinsnosa13@gmail.com>
2022-11-02 12:51:08 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int i = 0;
 
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
+	return (i);
 }
