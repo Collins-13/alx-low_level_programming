a9a327c589913152f1a3b6ae9ee0f41d84083cb4
Collins-13 <collinsnosa13@gmail.com>
2022-11-08 12:33:24 -0800

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,13 +9,13 @@
  * Return: a pointer to a 2 dimensional array of integers.
  */
 
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
 	int **array, i, j;
 	int len = width * height;
 
 	if (len <= 0)
-		return (NULL0;
+		return (NULL);
 
 	array = (int **)malloc(sizeof(int *) * height);
 	if (array == NULL)
@@ -23,14 +23,14 @@ int **alloc_grid(int width, int height);
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] += (int *)malloc(sizeof(int) * width);
+		array[i] = (int *)malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-				free(array[i]);	
+				free(array[i]);
 			free(array);
 			return (NULL);
-			}
+		}
 	}
 
 	for (i = 0; i < height; i++)
