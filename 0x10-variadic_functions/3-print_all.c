a64543bf361c2f122f136767903ee94c6201e808
Collins-13 <collinsnosa13@gmail.com>
2022-11-17 13:09:52 -0800

    task3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@ void print_all(const char * const format, ...)
 	int i;
 	int flag;
 	char *str;
-	va_lis a_list;
+	va_list a_list;
 
 	va_start(a_list, format);
 	i = 0;
@@ -19,26 +19,33 @@ void print_all(const char * const format, ...)
 	{
 		switch (format[i])
 		{
-		case 'c';
-			printf("%c", va_arg(a_list, int));
-			flag = 0;
-			break;
-		case 'i';
-			printf("%i", va_arg(a_list, int));
-			flag = 0;
-			break;
-		case 'f';
-			printf("%f", va_arg(a_list, double));
-			flag = 0;
-			break;
-		default:
-			flag = 1;
-			break;
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
 		}
 		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
 	printf("\n");
-	va_end(a-list);
+	va_end(a_list);
 }
