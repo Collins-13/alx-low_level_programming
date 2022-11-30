89417283d672e59b7f610a0a79a4cbbc10461535
Collins-13 <collinsnosa13@gmail.com>
2022-11-30 10:50:30 -0800

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,9 +23,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new;
+	*head = new;
 		return (new);
 	}
 	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
 	return (new);
 }
